ac02afe4e0db6b9f6a9851482ed5c20ae6693a83
Hoàng Cường <Cuonghoang23@users.noreply.github.com>
2025-11-04 19:39:41 +0700

    tutorial controller

---
+++ Assets/Script/TutorialController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        transform.position = Vector3.zero;
+    }
+
+    
+}
